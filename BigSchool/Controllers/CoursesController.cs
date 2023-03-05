1c123342a778f5c652105617ca23402756946edd
Nguyen Loan <Admin@LAPTOP-Q62KFUJF>
2023-03-05 17:10:01 +0700

    form create

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        private readonly ApplicationDbContext _dbcontext;
+        public CoursesController()
+        { 
+            _dbcontext= new ApplicationDbContext();
+        }
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbcontext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
