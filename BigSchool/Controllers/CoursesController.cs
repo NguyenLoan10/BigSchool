2496996d436809482f3f1ca54dab14b35618e9f9
Nguyen Loan <Admin@LAPTOP-Q62KFUJF>
2023-03-08 22:37:42 +0700

    style create

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool.Models;
 using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,7 @@ namespace BigSchool.Controllers
     public class CoursesController : Controller
     {
         // GET: Courses
-        private readonly ApplicationDbContext _dbcontext;
-        public CoursesController()
-        { 
-            _dbcontext= new ApplicationDbContext();
-        }
+        
         public ActionResult Create()
         {
             var viewModel = new CourseViewModel
@@ -24,5 +21,32 @@ namespace BigSchool.Controllers
             };
             return View(viewModel);
         }
+        private readonly ApplicationDbContext _dbcontext;
+        public CoursesController()
+        { 
+            _dbcontext= new ApplicationDbContext();
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbcontext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+              LecturerId = User.Identity.GetUserId(),
+              DateTime = viewModel.GetDateTime(),
+              CategoryId = viewModel.Category,
+              Place = viewModel.Place
+            };
+            _dbcontext.Courses.Add(course);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+       
     }
 }
